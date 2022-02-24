163caf49e0a95adbd6f0e5179cd10c79f68bd69d
JaiRaga <raga.j.santhosh@gmail.com>
2022-02-24 18:36:22 +0530

    :sparkles: added search bar and cart links and styled them for responsiveness

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,9 @@
 import Image from 'next/image'
+import {
+	MenuIcon,
+	SearchIcon,
+	ShoppingCartIcon,
+} from '@heroicons/react/outline'
 
 function Header() {
 	return (
@@ -13,6 +18,30 @@ function Header() {
 						objectFit='contain'
 						className='cursor-pointer'
 					/>
+				</div>
+				{/* Search bar */}
+				<div className='hidden sm:flex bg-yellow-400 hover:bg-yellow-500 items-center h-10 rounded-md flex-grow cursor-pointer'>
+					<input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type='text' />
+					<SearchIcon className='h-12 p-4' />
+				</div>
+
+				{/* right container */}
+				<div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+					<div className="link">
+						<p>Hello Raga Santhosh</p>
+						<p className="font-extrabold md:text-sm">Account & Lists</p>
+					</div>
+
+					<div className="link">
+						<p>Returns</p>
+						<p className="font-extrabold md:text-sm">& Orders</p>
+					</div>
+
+					<div className="relative link flex items-center">
+						<span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+						<ShoppingCartIcon className="h-10" />
+						<p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+					</div>
 				</div>
 			</div>
 
